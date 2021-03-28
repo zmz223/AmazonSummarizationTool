2b7b948eeaddda62aec205b82a795bf21b07f1c3
neil-mosca <62356064+neil-mosca@users.noreply.github.com>
2021-03-28 13:00:31 -0400

    This adds the ability to given an initial search result page, iterate through all pages to get all products from that search.

--- ASIN_SCRAPER/ASIN_SCRAPER/spiders/asin_spider.py
+++ ASIN_SCRAPER/ASIN_SCRAPER/spiders/asin_spider.py
@@ -1,14 +1,16 @@
-import scrapy
 import re
+import scrapy
 
 class ASIN_Spider(scrapy.Spider):
     name = "ASIN_Spider"
     start_urls = ['https://www.amazon.com/s?k=water&ref=nb_sb_noss_2']
-
     def __int__(self, query):
         self.query = query
 
     def parse(self, response):
+        allASINDivs = response.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/span[3]/div[2]")
+        pagination = allASINDivs.xpath('//li[@class="a-last"]')
+        link = pagination.css('a::attr(href)').get()
         ASIN_dict = {}
         for ind in range(2, 200):
             try:
@@ -18,9 +20,11 @@ class ASIN_Spider(scrapy.Spider):
             except Exception:
                 with open("../../asins.txt", "w") as f:
                     for item in ASIN_dict:
+                        #print(ASIN_dict[item])
                         f.write(item+":\t" + ASIN_dict[item]+"\n")
                 break
 
-
-
-
+        next_page = link
+        if next_page is not None:
+            next_page = "https://www.amazon.com/" + link
+            yield response.follow(next_page, callback=self.parse)
