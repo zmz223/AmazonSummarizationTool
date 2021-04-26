bd1c91ba3ed64e0ecc29006e85d63771a90a3e02
Zach Zaiman <zzaiman@emory.edu>
2021-04-25 22:59:28 -0400

    modified scraper and prelim reviews

--- ASIN_SCRAPER/ASIN_SCRAPER/spiders/review_spider.py
+++ ASIN_SCRAPER/ASIN_SCRAPER/spiders/review_spider.py
@@ -1,13 +1,24 @@
 import os
 import re
+import numpy as np
 import scrapy
 import psycopg2
+import pandas as pd
+
+def get_asins(csv_path):
+    data = pd.read_csv(csv_path)
+    return list(np.array(data)[:, 0])
+
 
 class ASIN_Spider(scrapy.Spider):
-
     name = "Review_Spider"
-    ASINS = ['B00JR5ADG0']
-    start_urls = ['https://www.amazon.com/Hamilton-Beach-73400-Popcorn-Popper/dp/B00JR5ADG0/ref=sr_1_51?dchild=1&keywords=popcorn+maker&qid=1619199982&sr=8-51#customerReviews']
+    ASINS = get_asins("links, asins, and categories.csv")
+    start_urls = [
+        'https://www.amazon.com/Hamilton-Beach-73400-Popcorn-Popper/dp/'+asin+'/ref=sr_1_51?dchild=1&keywords'
+        '=popcorn+maker&qid=1619199982&sr=8-51#customerReviews' for asin in ASINS]
+
+    row_list = []
+    productInfoData = []
 
     def __int__(self, query):
         self.query = query
@@ -28,21 +39,25 @@ class ASIN_Spider(scrapy.Spider):
             price = response.xpath("//span[@id='priceblock_ourprice']/text()").get()
             if price is None:
                 price = response.xpath("//span[@id='priceblock_saleprice']/text()").get()
-            producttableList = response.xpath("//table[@id='productDetails_detailBullets_sections1']//th/text()").getall()
+            producttableList = response.xpath(
+                "//table[@id='productDetails_detailBullets_sections1']//th/text()").getall()
             for temp in producttableList:
                 index = producttableList.index(temp)
                 producttableList[index] = temp.strip()
                 if producttableList[index] == 'Best Sellers Rank':
-                    tempString = "//table[@id='productDetails_detailBullets_sections1']//tr[" + str(index + 1) + "]/td/span/span[2]/text()"
+                    tempString = "//table[@id='productDetails_detailBullets_sections1']//tr[" + str(
+                        index + 1) + "]/td/span/span[2]/text()"
                     string1 = response.xpath(tempString).get()
-                    tempString2 = "//table[@id='productDetails_detailBullets_sections1']//tr[" + str(index + 1) + "]/td/span/span[2]/a/text()"
+                    tempString2 = "//table[@id='productDetails_detailBullets_sections1']//tr[" + str(
+                        index + 1) + "]/td/span/span[2]/a/text()"
                     string2 = response.xpath(tempString2).get()
                     BestSellerRank = string1 + string2
 
             aTag = response.xpath("//a[@data-hook='see-all-reviews-link-foot']")
             link = aTag.xpath("@href").extract()[0]
             allReviews = "https://www.amazon.com/" + str(link)
-            yield response.follow(allReviews, callback=self.parse, meta={'ASIN':ASIN, 'title':title,'price': price, 'BestSellerRank': BestSellerRank})
+            yield response.follow(allReviews, callback=self.parse,
+                                  meta={'ASIN': ASIN, 'title': title, 'price': price, 'BestSellerRank': BestSellerRank})
         except:
             productStar = response.xpath("//span[@data-hook='rating-out-of-text']/text()").get()
             numRatings = response.xpath("//span[@class='a-size-base a-color-secondary']/text()").get()
@@ -66,64 +81,54 @@ class ASIN_Spider(scrapy.Spider):
 
             reviewIDList = response.xpath("//div[@data-hook='review']/@id").getall()
             for ID in reviewIDList:
-                reviewTitle = response.xpath("//div[@id=\'" + ID + "\']//a[@data-hook='review-title']/span/text()").get()
-                reviewStars = response.xpath("//div[@id=\'" + ID + "\']//i[@data-hook='review-star-rating']/span/text()").get()
+                reviewTitle = response.xpath(
+                    "//div[@id=\'" + ID + "\']//a[@data-hook='review-title']/span/text()").get()
+                reviewStars = response.xpath(
+                    "//div[@id=\'" + ID + "\']//i[@data-hook='review-star-rating']/span/text()").get()
                 reviewDate = response.xpath("//div[@id=\'" + ID + "\']//span[@data-hook='review-date']/text()").get()
-                reviewNumHelp = response.xpath("//div[@id=\'" + ID + "\']//span[@data-hook='helpful-vote-statement']/text()").get()
+                reviewNumHelp = response.xpath(
+                    "//div[@id=\'" + ID + "\']//span[@data-hook='helpful-vote-statement']/text()").get()
                 reviewVerified = None
                 try:
-                    reviewVerified = response.xpath("//div[@id=\'" + ID + "\']//span[@data-hook='avp-badge']/text()").get()
+                    reviewVerified = response.xpath(
+                        "//div[@id=\'" + ID + "\']//span[@data-hook='avp-badge']/text()").get()
                 except:
                     reviewVerified = None
-                reviewComment = response.xpath("//div[@id=\'" + ID + "\']//span[@data-hook='review-body']/span/text()").get()
+                reviewComment = response.xpath(
+                    "//div[@id=\'" + ID + "\']//span[@data-hook='review-body']/span/text()").get()
                 reviewComment = reviewComment.strip()
+                temp_row = {}
+                temp_row["path"] = response.meta['ASIN']
+                temp_row["id"] = ID
+                temp_row["review_title"] = reviewTitle
+                temp_row["stars"] = reviewStars
+                temp_row["reviewDate"] = reviewDate
+                temp_row["reviewNumHelp"] = reviewNumHelp
+                temp_row["reviewVerified"] = reviewVerified
+                temp_row["reviewComment"] = reviewComment
 
-                con = psycopg2.connect(
-                    database="amazonProducts",
-                    user="postgres",
-                    password="$daxc8pofg!",
-                    host="localhost",
-                    port=5432
-                )
-
-                cur = con.cursor()
-
-                try:
-                    cur.execute("insert into reviews (asin, comment_id, title, star_rating, date, num_helpful, verified, comment) values (%s, %s, %s, %s, %s, %s, %s, %s)", (response.meta['ASIN'], ID, reviewTitle, reviewStars, reviewDate, reviewNumHelp, reviewVerified, reviewComment))
-                    con.commit()
-
-                except Exception as e:
-                    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-                    message = template.format(type(e).__name__, e.args)
-                    print(message)
-
-                cur.close()
-
-            con = psycopg2.connect(
-                database="amazonProducts",
-                user="postgres",
-                password="$daxc8pofg!",
-                host="localhost",
-                port=5432
-            )
-
-            cur = con.cursor()
-
-            try:
-                cur.execute("insert into product_info (asin, name, price, star_rating, num_ratings, product_rank, five_star, four_star, three_star, two_star, one_star) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (response.meta['ASIN'], response.meta['title'], response.meta['price'], productStar, numRatings, response.meta['BestSellerRank'], FiveStar, FourStar, ThreeStar, TwoStar, OneStar))
-                con.commit()
-
-            except Exception as e:
-                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-                message = template.format(type(e).__name__, e.args)
-                print(message)
-
-            cur.close()
+                self.row_list.append(temp_row)
+            # print(response.meta['ASIN'])
+            # print(response.meta['title'])
+            # print(response.meta['price'])
+            # print(productStar)
+            # print(numRatings)
+            # print(response.meta['BestSellerRank'])
+            # print(FiveStar)
+            # print(FourStar)
+            # print(ThreeStar)
+            # print(TwoStar)
+            # print(OneStar)
 
             pagination = response.xpath('//li[@class="a-last"]')
             link = pagination.css('a::attr(href)').get()
             next_page = link
             if next_page is not None:
                 next_page = "https://www.amazon.com/" + link
-                yield response.follow(next_page, callback=self.parse, meta={'ASIN':response.meta['ASIN'], 'title':response.meta['title'],'price': response.meta['price'], 'BestSellerRank': response.meta['BestSellerRank']})
+                csv = pd.DataFrame(self.row_list).to_csv("reviews.csv")
+                yield response.follow(next_page, callback=self.parse,
+                                      meta={'ASIN': response.meta['ASIN'], 'title': response.meta['title'],
+                                            'price': response.meta['price'],
+                                            'BestSellerRank': response.meta['BestSellerRank']})
+
 
